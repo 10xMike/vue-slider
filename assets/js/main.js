cf25c1c809a4be6ffd4bbb784c98a8035e44e599
10xMike <michelegec@gmail.com>
2021-05-20 20:24:50 +0200

    Pulizia del codice

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,55 +8,51 @@ Bonus:
 Applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente (usate created o mounted!). */
 
 const app = new Vue({
-    el: '#app',
-    data:{
-        counter: 0,
-        images: [
-            './assets/img/img (1).jpg',
-            './assets/img/img (2).jpg',
-            './assets/img/img (3).jpg',
-            './assets/img/img (4).jpg',
-            './assets/img/img (5).jpg',
-        ]
+  el: "#app",
+  data: {
+    counter: 0,
+    images: [
+      "./assets/img/img (1).jpg",
+      "./assets/img/img (2).jpg",
+      "./assets/img/img (3).jpg",
+      "./assets/img/img (4).jpg",
+      "./assets/img/img (5).jpg",
+    ],
+  },
+
+  methods: {
+    prev() {
+      console.log("click su prev");
+      if (this.counter === 0) {
+        return (this.counter = this.images.length - 1);
+      }
+      return (this.counter -= 1);
+    },
+    next() {
+      console.log("click su next");
+      if (this.counter === this.images.length - 1) {
+        return (this.counter = 0);
+      }
+      return (this.counter += 1);
     },
 
-    methods:{
-        prev(){
-            console.log('click su prev');
-            if(this.counter === 0){
-                return this.counter = this.images.length - 1
-            } 
-                return this.counter -= 1;
-        
-        },
-        next(){
-            console.log('click su next');
-            if(this.counter === this.images.length - 1){
-                return this.counter = 0;
-            }
-            return this.counter += 1;
-        },
-
-        selectImage(index){
-         return this.counter = index;
-        },   
+    selectImage(index) {
+      return (this.counter = index);
     },
+  },
 
-    
-
-    mounted(){
-        
-        document.addEventListener('keyup', (e) =>{
-            if (e.key === 'ArrowRight'){
-                this.next();
-            }
-            if (e.key === 'ArrowLeft');{
-                this.prev();
-            }
-        })
-    },
-    mounted(){
-        setInterval(this.next, 5000)
-    }
+  mounted() {
+    document.addEventListener("keyup", (e) => {
+      if (e.key === "ArrowRight") {
+        this.next();
+      }
+      if (e.key === "ArrowLeft");
+      {
+        this.prev();
+      }
+    });
+  },
+  mounted() {
+    setInterval(this.next, 5000);
+  },
 });
-
